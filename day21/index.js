1c97cd2c37650892484856d30cdaccf834a1d6de
uglyding2 <uglyding2@naver.com>
2019-01-02 17:10:28 +0900

    21회차 : 청남대 메인페이지

---
+++ day21/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.ham-menu').click(function(){
+        $('.sidebar').animate({right:'0'},1000);
+    });
+
+    $('.sidebar-close').click(function(){
+        $('.sidebar').animate({right:'-200px'},1000);
+    });
+
+});
