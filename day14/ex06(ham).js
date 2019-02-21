60bd9a0558575200fe96888266637650b1f45b82
uglyding2 <uglyding2@naver.com>
2019-02-21 16:30:29 +0900

    수정

---
+++ day14/ex06(ham).js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#ham').click(function(){
+        $('#sub-menu').toggleClass('display-block');
+    });
+    $(window).resize(function(){
+        if($(window).width()>640){
+            $('#sub-menu').removeClass('display-block');
+        }
+    });
+});
